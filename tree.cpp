c87436b5f6870084ff1967f9051cbda49d3dcabd
Shushmencev <shushmencev97@mail.ru>
2017-05-19 19:01:47 +0600

    first commit

---
+++ tree.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Tree{
+	
+	private:
+		
+		struct Multi_Node{
+			int data;
+			Multi_Node *parent;
+			vector <Multi_Node*> children;
+		};
+		
+	public:
+		
+		
+	
+};
+
+
+
+int main (){
+	
+	cout << "Hello world!!" << endl;
+	
+	return 0;
+}
