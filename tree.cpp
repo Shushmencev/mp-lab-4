e4ebbd83ee5f27079b5e6faa4ce75acbf68a5f19
Shushmencev <shushmencev97@mail.ru>
2017-05-23 19:21:46 +0600

    commit 3

--- tree.cpp
+++ tree.cpp
@@ -10,17 +10,40 @@ class Tree{
 		
 		struct Node{
 			int data;
-			Node *parent, *left, *right;
+			Node *left, *right;
 		};
 		
 	public:
-		
+		Node *root;	
 		Tree(){
+			
+			
 			root = NULL;
 		}
 		
 		~Tree(){
 			
+		}
+		
+		void add_node(int x){
+			
+			if (root == NULL){
+				
+				root = new Node;
+				root->data = x;
+				cout << x << endl;
+				root->left = root->right = NULL;
+			}
+			
+			if (root != NULL){
+				
+				if (x > root->data){
+					
+					root->left = new Node;
+					root->left->data = x;
+					root->left->left = root->left->right = NULL;
+				}
+			}
 		}
 		
 		int get_node_depth(){
@@ -33,7 +56,8 @@ class Tree{
 
 int main(){
 	
-	
+	Tree my_tree;
+	my_tree.add_node(5);
 	
 	return 0;
 }
